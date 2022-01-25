42e2bc34eb67878e3f420e1f29e501befefe82bf
mohahmedkhan <93582069+mohahmedkhan@users.noreply.github.com>
2022-01-25 16:01:10 +0000

    Add files via upload

---
+++ concantenation.py.py
@@ -0,0 +1,15 @@
+youtuber = "ahmed"
+print("subscribe to {}".format(youtuber))
+print("subscribe to " + youtuber)
+print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input("famous person: ")
+madlib = f"computer programming is so {adj}! It makes me so excited all the \
+time because I love to {verb1}. Stay hydrated and {verb2} like you are \
+{famous_person}!"
+
+print(madlib)
+
